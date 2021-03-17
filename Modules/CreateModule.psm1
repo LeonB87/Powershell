710d0592f8f4822e0021ffcd30f60dd7a3f1a711
Léon <leonboers@gmail.com>
2021-03-17 13:58:38 +0100

    cleanup old useless scripts

--- Modules/CreateModule.psm1
+++
@@ -1,7 +0,0 @@
-$manifest = @{
-    Path          = '.\Computer Information\Computer Information.psd1'
-    RootModule    = 'Computer Information.psm1'
-    Author        = 'Leon Boers'
-    ModuleVersion = "1.0"
-}
-New-ModuleManifest @manifest
