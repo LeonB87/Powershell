e844d40ea9bb7ac87c4026f2a1696420e3e608c2
Léon <admin@familieboers.onmicrosoft.com>
2021-03-20 11:34:34 +0100

    update powershell documentation generation

--- utilities/New-MDPowerShellScripts.ps1
+++ utilities/New-MDPowerShellScripts.ps1
@@ -1,4 +1,5 @@
-<#
+function wiki {
+    <#
 .SYNOPSIS
     Script for generating Markdown documentation based on information in PowerShell script files.
 
@@ -31,153 +32,192 @@ Include the TOC from the Azure DevOps wiki to the markdown files
 .EXAMPLE
     .\New-MDPowerShellScripts.ps1 -ScriptFolder "./" -OutputFolder "docs/powershell"
 #>
-[CmdletBinding()]
+    [CmdletBinding()]
 
-Param (
-    [Parameter(Mandatory = $true, Position = 0)][string]$ScriptFolder,
-    [Parameter(Mandatory = $true, Position = 1)][string]$OutputFolder,
-    [Parameter(Mandatory = $false, Position = 2)][string]$ExcludeFolders,
-    [Parameter(Mandatory = $false, Position = 3)][bool]$KeepStructure = $false,
-    [Parameter(Mandatory = $false, Position = 4)][bool]$IncludeWikiTOC = $false
-)
+    Param (
+        [Parameter(Mandatory = $true, Position = 0)][string]$ScriptFolder,
+        [Parameter(Mandatory = $true, Position = 1)][string]$OutputFolder,
+        [Parameter(Mandatory = $false, Position = 2)][string]$ExcludeFolders,
+        [Parameter(Mandatory = $false, Position = 3)][bool]$KeepStructure = $false,
+        [Parameter(Mandatory = $false, Position = 4)][bool]$IncludeWikiTOC = $false,
+        [Parameter(Mandatory = $false, Position = 5)][bool]$IncludeWikiSummary = $false,
+        [Parameter(Mandatory = $false, Position = 6)][string]$WikiSummaryOutputfileName
+    )
 
-BEGIN {
-    Write-Output ("ScriptFolder         : $($ScriptFolder)")
-    Write-Output ("OutputFolder         : $($OutputFolder)")
-    Write-Output ("ExcludeFolders       : $($ExcludeFolders)")
-    Write-Output ("KeepStructure        : $($KeepStructure)")
-    Write-Output ("IncludeWikiTOC       : $($IncludeWikiTOC)")
+    BEGIN {
+        Write-Output ("ScriptFolder                 : $($ScriptFolder)")
+        Write-Output ("OutputFolder                 : $($OutputFolder)")
+        Write-Output ("ExcludeFolders               : $($ExcludeFolders)")
+        Write-Output ("KeepStructure                : $($KeepStructure)")
+        Write-Output ("IncludeWikiTOC               : $($IncludeWikiTOC)")
+        Write-Output ("IncludeWikiSummary           : $($IncludeWikiSummary)")
+        Write-Output ("WikiSummaryOutputfileName    : $($WikiSummaryOutputfileName)")
 
-    $arrParameterProperties = @("DefaultValue", "ParameterValue", "PipelineInput", "Position", "Required")
-    $scriptNameSuffix = ".md"
-    $option = [System.StringSplitOptions]::RemoveEmptyEntries
+        $arrParameterProperties = @("DefaultValue", "ParameterValue", "PipelineInput", "Position", "Required")
+        $scriptNameSuffix = ".md"
+        $option = [System.StringSplitOptions]::RemoveEmptyEntries
 
-    $exclude = $ExcludeFolders.Split(',', $option)
+        $exclude = $ExcludeFolders.Split(',', $option)
 
-}
-PROCESS {
-    try {
-        Write-Information ("Starting documentation generation for folder $($ScriptFolder)")
+    }
+    PROCESS {
+        try {
+            Write-Information ("Starting documentation generation for folder $($ScriptFolder)")
 
-        if (!(Test-Path $OutputFolder)) {
-            Write-Information ("Output path does not exists creating the folder: $($OutputFolder)")
-            New-Item -ItemType Directory -Force -Path $OutputFolder
-        }
+            if (!(Test-Path $OutputFolder)) {
+                Write-Information ("Output path does not exists creating the folder: $($OutputFolder)")
+                New-Item -ItemType Directory -Force -Path $OutputFolder
+            }
 
-        # Get the scripts from the folder
-        $scripts = Get-Childitem $ScriptFolder -Filter "*.ps1" -Recurse
+            # Get the scripts from the folder
+            $scripts = Get-Childitem $ScriptFolder -Filter "*.ps1" -Recurse
 
-        foreach ($script in $scripts) {
-            if (!$exclude.Contains($script.Directory.Name)) {
-                Write-Information ("Documenting file: $($script.FullName)")
-
-
-                if ($KeepStructure) {
-                    if ($script.DirectoryName -ne $ScriptFolder) {
-                        $newfolder = $OutputFolder + "/" + $script.Directory.Name
-                        if (!(Test-Path $newfolder)) {
-                            Write-Information ("Output folder for item does not exists creating the folder: $($newfolder)")
-                            New-Item -Path $OutputFolder -Name $script.Directory.Name -ItemType "directory"
-                        }
-                    }
-                } else {
-                    $newfolder = $OutputFolder
+            if ($IncludeWikiSummary) {
+                if ($WikiSummaryOutputfileName) {
+                    $WikiSummaryFilename = (".\$($WikiSummaryOutputfileName)")
+                }
+                else {
+                    $WikiSummaryFilename = (".\powershellScripts.md")
                 }
 
-                $help = Get-Help $script.FullName -ErrorAction "SilentlyContinue" -Detailed
+                if ($IncludeWikiTOC) {
+                    ("[[_TOC_]]`n") | Out-File -FilePath $WikiSummaryFilename
+                    "`n" | Out-File -FilePath $WikiSummaryFilename -Append
+                }
+            }
 
-                if ($help) {
-                    $outputFile = ("$($newfolder)/$($script.BaseName)$($scriptNameSuffix)")
-                    Out-File -FilePath $outputFile
 
-                    if ($IncludeWikiTOC) {
-                        ("[[_TOC_]]`n") | Out-File -FilePath $outputFile
-                        "`n" | Out-File -FilePath $outputFile -Append
-                    }
+            foreach ($script in $scripts) {
+                if (!$exclude.Contains($script.Directory.Name)) {
+                    Write-Information ("Documenting file: $($script.FullName)")
 
-                    #synopsis
-                    if ($help.Synopsis) {
-                        ("## Synopsis") | Out-File -FilePath $outputFile -Append
-                        ("$($help.Synopsis)") | Out-File -FilePath $outputFile -Append
-                        "`n" | Out-File -FilePath $outputFile -Append
-                    } else {
-                        Write-Warning -Message ("Synopsis not defined in file $($script.fullname)")
-                    }
 
-                    #syntax
-                    if ($help.Syntax) {
-                        $capturedGetHelpOutput = $help.Syntax | Out-String
-                        $parameters = $capturedGetHelpOutput.split($script.name).Trim()[1]
-                        $syntaxString = (".\$($script.name) $($parameters)")
-                        ("``````PowerShell`n $($syntaxString)`n``````") | Out-File -FilePath $outputFile -Append
-                        "`n" | Out-File -FilePath $outputFile -Append
-                    } else {
-                        Write-Warning -Message ("Syntax not defined in file $($script.fullname)")
-                    }
-
-                    #notes (seperated by (name): and (value);)
-                    if ($help.alertSet) {
-                        ("## Information") | Out-File -FilePath $outputFile -Append
-                        $text = $help.alertSet.alert.Text.Split(';', $option)
-                        foreach ($line in $text) {
-                            $items = $line.Trim().Split(':', $option)
-                            ("**$($items[0]):** $($items[1])`n") | Out-File -FilePath $outputFile -Append
-                        }
-                        "`n" | Out-File -FilePath $outputFile -Append
-                    } else {
-                        Write-Warning -Message ("Notes not defined in file $($script.fullname)")
-                    }
-
-                    #description
-                    if ($help.Description) {
-                        "## Description" | Out-File -FilePath $outputFile -Append
-                        $help.Description.Text | Out-File -FilePath $outputFile -Append
-                        "`n" | Out-File -FilePath $outputFile -Append
-                    } else {
-                        Write-Warning -Message "Description not defined in file $($script.fullname)"
-                    }
-
-                    #examples
-                    if ($help.Examples) {
-                        ("## Examples") | Out-File -FilePath $outputFile -Append
-                        "`n" | Out-File -FilePath $outputFile -Append
-                        forEach ($item in $help.Examples.Example) {
-                            $title = $item.title.Replace("--------------------------", "").Replace("EXAMPLE", "Example")
-                            ("### $($title)") | Out-File -FilePath $outputFile -Append
-                            if ($item.Code) {
-                                ("``````PowerShell`r`n $($item.Code) `r`n``````") | Out-File -FilePath $outputFile -Append
+                    if ($KeepStructure) {
+                        if ($script.DirectoryName -ne $ScriptFolder) {
+                            $newfolder = $OutputFolder + "/" + $script.Directory.Name
+                            if (!(Test-Path $newfolder)) {
+                                Write-Information ("Output folder for item does not exists creating the folder: $($newfolder)")
+                                New-Item -Path $OutputFolder -Name $script.Directory.Name -ItemType "directory"
                             }
                         }
-                    } else {
-                        Write-Warning -Message "Examples not defined in file $($script.fullname)"
+                    }
+                    else {
+                        $newfolder = $OutputFolder
                     }
 
-                    if ($help.Parameters) {
-                        ("## Parameters") | Out-File -FilePath $outputFile -Append
-                        forEach ($item in $help.Parameters.Parameter) {
-                            ("### $($item.name)") | Out-File -FilePath $outputFile -Append
-                            $item.description[0].text | Out-File -FilePath $outputFile -Append
-                            ("| | |") | Out-File -FilePath $outputFile -Append
-                            ("|-|-|") | Out-File -FilePath $outputFile -Append
-                            ("| Type: | $($item.Type.Name) |") | Out-File -FilePath $outputFile -Append
-                            foreach ($arrParameterProperty in $arrParameterProperties) {
-                                if ($item.$arrParameterProperty) {
-                                    ("| $arrParameterProperty : | $($item.$arrParameterProperty)|") | Out-File -FilePath $outputFile -Append
-                                }
+                    $help = Get-Help $script.FullName -ErrorAction "SilentlyContinue" -Detailed
+
+                    if ($IncludeWikiSummary) {
+                        ("# $($script.Name) `r ") | Out-File -FilePath $WikiSummaryFilename -Append
+                    }
+
+                    if ($help) {
+                        $outputFile = ("$($newfolder)/$($script.BaseName)$($scriptNameSuffix)")
+                        Out-File -FilePath $outputFile
+
+                        if ($IncludeWikiTOC) {
+                            ("[[_TOC_]]`n") | Out-File -FilePath $outputFile
+                            "`n" | Out-File -FilePath $outputFile -Append
+                        }
+
+                        #synopsis
+                        if ($help.Synopsis) {
+                            ("## Synopsis") | Out-File -FilePath $outputFile -Append
+                            ("$($help.Synopsis)") | Out-File -FilePath $outputFile -Append
+                            "`n" | Out-File -FilePath $outputFile -Append
+
+
+                            if ($IncludeWikiSummary) {
+                                ("$($help.Synopsis)") | Out-File -FilePath $WikiSummaryFilename -Append
+                            }
+                        }
+                        else {
+                            Write-Warning -Message ("Synopsis not defined in file $($script.fullname)")
+                        }
+
+                        #syntax
+                        if ($help.Syntax) {
+                            $capturedGetHelpOutput = $help.Syntax | Out-String
+                            $parameters = $capturedGetHelpOutput.split($script.name).Trim()[1]
+                            $syntaxString = (".\$($script.name) $($parameters)")
+                            ("``````PowerShell`n $($syntaxString)`n``````") | Out-File -FilePath $outputFile -Append
+                            "`n" | Out-File -FilePath $outputFile -Append
+                        }
+                        else {
+                            Write-Warning -Message ("Syntax not defined in file $($script.fullname)")
+                        }
+
+                        #notes (seperated by (name): and (value);)
+                        if ($help.alertSet) {
+                            ("## Information") | Out-File -FilePath $outputFile -Append
+                            $text = $help.alertSet.alert.Text.Split(';', $option)
+                            foreach ($line in $text) {
+                                $items = $line.Trim().Split(':', $option)
+                                ("**$($items[0]):** $($items[1])`n") | Out-File -FilePath $outputFile -Append
                             }
                             "`n" | Out-File -FilePath $outputFile -Append
                         }
-                    } else {
-                        Write-Warning -Message "Parameters not defined in file $($script.fullname)"
-                    }
+                        else {
+                            Write-Warning -Message ("Notes not defined in file $($script.fullname)")
+                        }
 
-                } else {
-                    Write-Error -Message ("Synopsis could not be found for script $($script.FullName)")
+                        #description
+                        if ($help.Description) {
+                            "## Description" | Out-File -FilePath $outputFile -Append
+                            $help.Description.Text | Out-File -FilePath $outputFile -Append
+                            "`n" | Out-File -FilePath $outputFile -Append
+                        }
+                        else {
+                            Write-Warning -Message "Description not defined in file $($script.fullname)"
+                        }
+
+                        #examples
+                        if ($help.Examples) {
+                            ("## Examples") | Out-File -FilePath $outputFile -Append
+                            "`n" | Out-File -FilePath $outputFile -Append
+                            forEach ($item in $help.Examples.Example) {
+                                $title = $item.title.Replace("--------------------------", "").Replace("EXAMPLE", "Example")
+                                ("### $($title)") | Out-File -FilePath $outputFile -Append
+                                if ($item.Code) {
+                                    ("``````PowerShell`r`n $($item.Code) `r`n``````") | Out-File -FilePath $outputFile -Append
+                                }
+                            }
+                        }
+                        else {
+                            Write-Warning -Message "Examples not defined in file $($script.fullname)"
+                        }
+
+                        if ($help.Parameters) {
+                            ("## Parameters") | Out-File -FilePath $outputFile -Append
+                            forEach ($item in $help.Parameters.Parameter) {
+                                ("### $($item.name)") | Out-File -FilePath $outputFile -Append
+                                $item.description[0].text | Out-File -FilePath $outputFile -Append
+                                ("| | |") | Out-File -FilePath $outputFile -Append
+                                ("|-|-|") | Out-File -FilePath $outputFile -Append
+                                ("| Type: | $($item.Type.Name) |") | Out-File -FilePath $outputFile -Append
+                                foreach ($arrParameterProperty in $arrParameterProperties) {
+                                    if ($item.$arrParameterProperty) {
+                                        ("| $arrParameterProperty : | $($item.$arrParameterProperty)|") | Out-File -FilePath $outputFile -Append
+                                    }
+                                }
+                                "`n" | Out-File -FilePath $outputFile -Append
+                            }
+                        }
+                        else {
+                            Write-Warning -Message "Parameters not defined in file $($script.fullname)"
+                        }
+
+                    }
+                    else {
+                        Write-Error -Message ("Synopsis could not be found for script $($script.FullName)")
+                    }
                 }
             }
         }
-    } catch {
-        Write-Error "Something went wrong while generating the output documentation: $_"
+        catch {
+            Write-Error "Something went wrong while generating the output documentation: $_"
+        }
     }
+    END {}
 }
-END {}
+wiki -ScriptFolder .\Powershell\Scripts\ -KeepStructure $false -IncludeWikiTOC $true -OutputFolder .\docs -IncludeWikiSummary $true -WikiSummaryOutputfileName
