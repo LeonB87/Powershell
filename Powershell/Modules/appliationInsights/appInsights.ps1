7094b2331b9b5e8ce6f7797a7a11c5ce684e73ce
Léon <leonboers@gmail.com>
2022-06-05 21:34:03 +0200

    working

--- Powershell/Modules/appliationInsights/appInsights.ps1
+++ Powershell/Modules/appliationInsights/appInsights.ps1
@@ -1,7 +1,3 @@
-
-
-
-
 function New-ApplicationInsightsClient {
     [CmdletBinding()]
     param (
@@ -11,10 +7,52 @@ function New-ApplicationInsightsClient {
         $InstrumentationKey
     )
 
-    $AIClient = [Microsoft.ApplicationInsights.TelemetryClient]::new()
-    $AIClient.InstrumentationKey = $InstrumentationKey
+    $Client = [Microsoft.ApplicationInsights.TelemetryClient]::new()
+    $Client.InstrumentationKey = $InstrumentationKey
 
-    return $AIClient
+    $defaultUserInformation = @{
+        AuthenticatedUserId = [Security.Principal.WindowsIdentity]::GetCurrent().Name
+        UserAgent           = ("PS $($psversiontable.PSEdition) $($psversiontable.PSVersion)")
+    }
+
+    $defaultDeviceInformation = @{
+        ID              = (Get-CimInstance -Class Win32_ComputerSystemProduct).UUID
+        OperatingSystem = $psversiontable.OS
+
+    }
+
+    $client = Set-ApplicationInsightsClientInformation -UserInformation $defaultUserInformation -DeviceInformation $defaultDeviceInformation -Client $Client
+
+    return $Client
+}
+
+function Confirm-ApplicationInsightsClient {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [Microsoft.ApplicationInsights.TelemetryClient]
+        $Client
+    )
+
+    [bool] $isValid = $true
+    Write-Verbose ("Checking if the Application Insights Client is valid...")
+
+    if ([string]::IsNullOrWhiteSpace($client.InstrumentationKey)) {
+        Write-Verbose ("The Instrumentation Key is not set.")
+        $isValid = $false
+    }
+
+    if ($client.Isenabled() -eq $false) {
+        Write-Verbose ("The Application Insights Client is not enabled.")
+        $isValid = $false
+    }
+
+    if ($isValid -eq $true) {
+        Write-Verbose ("The Application Insights Client is valid.")
+    }
+    else {
+        throw ("The Application Insights Client is not valid.")
+    }
 }
 
 function Set-ApplicationInsightsClientInformation {
@@ -22,34 +60,54 @@ function Set-ApplicationInsightsClientInformation {
     param (
         [Parameter(Mandatory = $true)]
         [Microsoft.ApplicationInsights.TelemetryClient]
-        $AIClient,
+        $Client,
 
         [Parameter(Mandatory = $false)]
         [hashtable]
-        $User
+        $UserInformation,
+
+        [Parameter(Mandatory = $false)]
+        [hashtable]
+        $DeviceInformation
     )
     begin {
-        if (-not $null -eq $User) {
+        if (-not $null -eq $UserInformation) {
             Write-Verbose ("Received 'User' properties to set in the client")
+        }
+
+        if (-not $null -eq $DeviceInformation) {
+            Write-Verbose ("Received 'Device' properties to set in the client")
         }
     }
 
     process {
-        if (-not $null -eq $User) {
+        if (-not $null -eq $UserInformation) {
 
-            foreach ($property in $AIClient.Context.User.psobject.Properties.name) {
+            foreach ($property in $Client.Context.User.psobject.Properties.name) {
                 Write-Verbose ("Checking property '$($property)' in supplied hashtable")
-                if (-not [string]::IsNullOrWhiteSpace($User[$property])) {
-                    Write-Verbose ("Found property '$($property)' with a value. Changing value from '$($AIClient.Context.User.$property)' to '$($User[$property])'")
+                if (-not [string]::IsNullOrWhiteSpace($UserInformation[$property])) {
+                    Write-Verbose ("Found property '$($property)' with a value. Changing value from '$($Client.Context.User.$property)' to '$($UserInformation[$property])'")
 
-                    $AIClient.Context.User.$property = $User[$property]
+                    $Client.Context.User.$property = $UserInformation[$property]
+                }
+            }
+        }
+
+        if (-not $null -eq $DeviceInformation) {
+
+            foreach ($property in $Client.Context.Device.psobject.Properties.name) {
+                Write-Verbose ("Checking property '$($property)' in supplied hashtable")
+                if (-not [string]::IsNullOrWhiteSpace($DeviceInformation[$property])) {
+                    Write-Verbose ("Found property '$($property)' with a value. Changing value from '$($Client.Context.Device.$property)' to '$($DeviceInformation[$property])'")
+
+                    $Client.Context.Device.$property = $DeviceInformation[$property]
                 }
             }
         }
     }
 
     end {
-        return $AIClient
+        return $Client
     }
 
 }
@@ -80,6 +138,8 @@ function Write-ApplicationInsightsTrace {
         if ($properties.Count -ge 1) {
             Write-Verbose ("Received '$($properties.Count)' properties to add to the message.")
         }
+
+        Confirm-ApplicationInsightsClient $client
     }
     PROCESS {
         if ($properties.Count -ge 1) {
@@ -149,7 +209,7 @@ function Write-ApplicationInsightsException {
         [string]
         $ExceptionString,
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, HelpMessage = "This is a dictionary<string, double> with additional information that will be added as 'customMeasurements' in Application Insights")]
         [System.Collections.Generic.Dictionary[string, double]]
         $Metrics = [System.Collections.Generic.Dictionary[string, double]]::new(),
 
@@ -174,43 +234,44 @@ function Write-ApplicationInsightsException {
     }
 }
 
+function Write-ApplicationInsightRequest {
+
+}
+
 
 $client = New-ApplicationInsightsClient -InstrumentationKey c323cf10-da34-4a73-9eac-47dad64d840b
 Write-ApplicationInsightsTrace -Client $client -Message "This is a test message" -SeverityLevel "Information"
+
 
 $dictionary = [System.Collections.Generic.Dictionary[string, string]]::new()
 
 $dictionary.Add("FirstName", "John")
 $dictionary.Add("LastName", "Doe")
 
-Write-ApplicationInsightsTrace -Client $client -Message "This is a test message with properties" -SeverityLevel "Information" -properties $dictionary
+# Write-ApplicationInsightsTrace -Client $client -Message "This is a test message with properties" -SeverityLevel "Information" -properties $dictionary
 
-Write-ApplicationInsightsMetric -Client $client -Name 'My Metric Name' -Metric 100
-Write-ApplicationInsightsMetric -Client $client -Name 'My Metric Name' -Metric 100 -properties $dictionary
+# Write-ApplicationInsightsMetric -Client $client -Name 'My Metric Name' -Metric 100
+# Write-ApplicationInsightsMetric -Client $client -Name 'My Metric Name' -Metric 100 -properties $dictionary
 
-Write-ApplicationInsightsException -ExceptionString "This happend!" -Client $client -properties $dictionary
+# Write-ApplicationInsightsException -ExceptionString "This happend!" -Client $client -properties $dictionary
 
-$metrics = [System.Collections.Generic.Dictionary[string, double]]::new()
+# $metrics = [System.Collections.Generic.Dictionary[string, double]]::new()
 
-$metrics.Add("MyMetric", 100)
-$metrics.Add("MyMetric 2", 150)
+# $metrics.Add("MyMetric", 100)
+# $metrics.Add("MyMetric 2", 150)
 
-try {
-    0 / 0
-}
-catch {
-    $caughtException = $_.Exception
-}
+# try {
+#     0 / 0
+# }
+# catch {
+#     $caughtException = $_.Exception
+# }
 
-Write-ApplicationInsightsException -Exception $caughtException -Client $client -Metrics $metrics -properties $dictionary
+# Write-ApplicationInsightsException -Exception $caughtException -Client $client -Metrics $metrics -properties $dictionary
 
 
 # # PageView
 # $client.TrackPageView($MyInvocation.MyCommand.Name)
-# $Client.Flush()
-
-# # Exceptions
-# $client.TrackException([System.Exception]::new("Hello"))
 # $Client.Flush()
 
 # # Requests
