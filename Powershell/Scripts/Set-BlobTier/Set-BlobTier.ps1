22f46a96eba0f1e2c6a0851f877ef96cf5502afb
Léon <leonboers@gmail.com>
2022-10-29 16:07:07 +0200

    version 1.0.0 of Set-BlobTier script

--- Powershell/Scripts/Set-BlobTier/Set-BlobTier.ps1
+++ Powershell/Scripts/Set-BlobTier/Set-BlobTier.ps1
@@ -1,28 +1,89 @@
-$storageAccountName = ""     # Enter account name
-$storageContainer = ""           # Enter specific container
-$prefix = "a"                      # Set prefix for scanning
-$MaxReturn = 10000
-$count = 0
-$StorageAccountKey = "" # Enter account/sas key
 
-Write-Host "Starting script"
 
-$ctx = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $StorageAccountKey 
-$Token = $Null
+function Set-BlobTier {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNull()]
+        [string]$storageAccountName,
 
-do {  
-    $listOfBlobs = Get-AzStorageBlob -Container $storageContainer -Context $ctx -MaxCount $MaxReturn -ContinuationToken $Token -Prefix $prefix
-  
-    foreach ($blob in $listOfBlobs) {  
-        if ($blob.ICloudBlob.Properties.StandardBlobTier -eq "Archive") { 
-            $blob.ICloudBlob.SetStandardBlobTier("Hot")
-            #write-host "the blob " $blob.name " is being set to Hot"
-            $count++
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNull()]
+        [string]$storageContainer,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNull()]
+        [int]$MaxReturn = 10000,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNull()]
+        [string]$StorageAccountKey,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNull()]
+        [string]$regularExpression = "^2019",
+
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("Cold", "Hot", "Archive", "Any")]
+        [string]$sourceTier = "*",
+
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("Cold", "Hot", "Archive")]
+        [string]$targetTier = "Cold"
+    )
+    begin {
+
+        if ($sourceTier -eq $targetTier) {
+            Write-Error ("Sourcetier and target Tier are the same..")
+            Exit 1
         }
-    }  
-    $Token = $blob[$blob.Count - 1].ContinuationToken;  
-    
-    Write-Host "Processed " ($count) " items. Continuation token = " $Token.NextMarker
-}while ($Null -ne $Token)
 
-Write-Host "Complete processing of all blobs returned with prefix " $prefix
+        Write-Output ("Starting script")
+        Write-Output ("Searching container '$($storageContainer)' in storage account '$($storageAccountName)' with regularExpression '$($regularExpression)'")
+        Write-Output ("File with the source tier set to '$($sourceTier)' will be changed to '$($targetTier)'")
+    }
+    process {
+        $storageContext = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $StorageAccountKey
+
+        #TODO: Validate context
+
+        $Token = $Null
+
+        do {
+            $blobs = Get-AzStorageBlob -Container $storageContainer -Context $storageContext -MaxCount $MaxReturn -ContinuationToken $Token
+
+            :blobLoop foreach ($blob in $blobs) {
+                if ($targetTier -ne "Any") {
+                    if ($blob.ICloudBlob.Properties.StandardBlobTier -ine $source) {
+                        Write-Verbose ("The blob '$($blob.Name)' has the wrong source Tier")
+                        continue blobLoop
+                    }
+                }
+
+                if ($blob.ICloudBlob.Properties.StandardBlobTier -ieq $targetTier) {
+                    Write-Verbose ("The blob '$($blob.Name)' is already set to the tier '$($targetTier)'")
+                    continue blobLoop
+                }
+
+                if ($blob.Name -match $regularExpression) {
+                    $blob.ICloudBlob.SetStandardBlobTier($targetTier)
+                    $count++ | Out-Null
+                }
+                else {
+                    Write-Verbose ("The file '$blob.Name' does not match the regular expression '$($regularExpression)'")
+                    [int]$skipped++ | Out-Null
+                }
+
+            }
+            $Token = $blob[$blob.Count - 1].ContinuationToken;
+
+            Write-Output "Processed "($count)" items. Continuation token = " $Token.NextMarker
+        }while ($Null -ne $Token)
+    }
+
+    end {
+        Write-Output ("Converted '$($count)' files to Archive and skipped '$($skipped)'")
+    }
+}
+
+Set-BlobTier -regularExpression '^(202[0-1])|^201[0-9]' -storageAccountName 'azstorphotobackup' -storageContainer 'suzannefotografie' -StorageAccountKey $accessKey -sourceTier Any -targetTier Archive
